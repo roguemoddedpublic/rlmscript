c81b32f6d9ed2468bb85116c5c42004325c89183
aegpublic <colinholder99@gmail.com>
2025-05-22 17:34:19 -0700

    Update TrinketType.lua

--- Trinket_ESP/TrinketType.lua
+++ Trinket_ESP/TrinketType.lua
@@ -4,64 +4,14 @@ return function(Object: Instance)
 	local Particle: ParticleEmitter = Object:FindFirstChildWhichIsA('ParticleEmitter', true)
 	
 	if Object:IsA('MeshPart') then
-		if Object.MeshId == 'rbxassetid://5196551436' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Amulet'
-			}
-		elseif Object.MeshId == 'rbxassetid://923469333' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Candy'
-			}
-		elseif Object.MeshId == 'rbxassetid://5204003946' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Goblet'
-			}
-		elseif Object.MeshId == 'rbxassetid://2520762076' then
+		if Object.MeshId == 'rbxassetid://2520762076' then
 			return {
 				Color = Color3.fromRGB(137, 207, 240),
 				Name = 'Howler Friend'
 			}
-		elseif Object.MeshId == 'rbxassetid://5196577540' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Old Amulet'
-			}
-		elseif Object.MeshId == 'rbxassetid://5196782997' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Old Ring'
-			}
-		elseif Object.MeshId == 'rbxassetid://5196776695' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Ring'
-			}
-		elseif Object.MeshId == 'rbxassetid://5204453430' then
-			return {
-				Color = Color3.fromRGB(137, 207, 240),
-				Name = 'Scroll'
-			}
 		end
 	elseif Object:IsA('Part') then
-		if Object.Color == Color3.fromRGB(89, 34, 89) then
-			return {
-				Color = Color3.fromRGB(137, 207, 240),
-				Name = '???'
-			}
-		elseif Object.Color == Color3.fromRGB(164, 187, 190) then
-			return {
-				Color = Color3.fromRGB(137, 207, 240),
-				Name = 'Diamond'
-			}
-		elseif Object.Color == Color3.fromRGB(0, 184, 49) then
-			return {
-				Color = Color3.fromRGB(137, 207, 240),
-				Name = 'Emerald'
-			}
-		elseif Object.Color == Color3.fromRGB(128, 187, 219) then
+		if Object.Color == Color3.fromRGB(128, 187, 219) then
 			return {
 				Color = Color3.fromRGB(255, 0, 255),
 				Name = 'Fairfrozen'
@@ -76,11 +26,6 @@ return function(Object: Instance)
 				Color = Color3.fromRGB(255, 213, 128),
 				Name = 'Mysterious Artifact'
 			}
-		elseif Object.Color == Color3.fromRGB(248, 248, 248) and Mesh then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Opal'
-			}
 		elseif Particle and Particle.Color.Keypoints[1].Value == Color3.new(1, 0.8, 0) then
 			return {
 				Color = Color3.fromRGB(137, 207, 240),
@@ -90,16 +35,6 @@ return function(Object: Instance)
 			return {
 				Color = Color3.fromRGB(255, 0, 255),
 				Name = 'Rift Gem'
-			}
-		elseif Object.Color == Color3.fromRGB(255, 0, 0) then
-			return {
-				Color = Color3.fromRGB(137, 207, 240),
-				Name = 'Ruby'
-			}
-		elseif Object.Color == Color3.fromRGB(16, 42, 220) then
-			return {
-				Color = Color3.fromRGB(137, 207, 240),
-				Name = 'Sapphire'
 			}
 		elseif Object.Color == Color3.fromRGB(255, 255, 0) then
 			return {
